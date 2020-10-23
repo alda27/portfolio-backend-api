51b588aaf72fd8ecc0e13d70adbaf26434c109ef
alda27 <amunozro8970@universidadean.edu.co>
2020-10-23 00:14:04 -0500

    created some test

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Project, Tags
+
 
 # Create your tests here.
+
+class ProjectModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Project.objects.create(name='test projects', description='Description test', url_project_online='www.test.com')
+
+    def test_title_content(self):
+        todo = Project.objects.get(name='test projects')
+        expected_object_name = f'{todo.name}'
+        self.assertEquals(expected_object_name, 'test projects')
