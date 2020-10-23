197b132bf0f3e563f50d321d5cec8a7a0d65c673
alda27 <amunozro8970@universidadean.edu.co>
2020-10-22 23:06:22 -0500

    added models tags and projects to the admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Tags, Project
+
+
 # Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'date_updated')
+    search_fields = ('name',)
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'date_updated')
+    search_fields = ('name',)
