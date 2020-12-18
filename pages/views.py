01a7f31177e83aa9173e3bdafea7c4f7bc71eb92
alda27 <amunozro8970@universidadean.edu.co>
2020-12-18 16:14:31 -0500

    added views for blog app

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from rest_framework import generics
-from projects import views
+from projects import views as project_views
+from blog import views as blog_views
 
 
 # Create your views here.
@@ -12,8 +13,9 @@ class ApiRoot(generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return Response({
-            'projects': reverse(views.ListProject.name, request=request),
-            'projects-tags': reverse(views.ListTags.name, request=request)
+            'projects': reverse(project_views.ListProject.name, request=request),
+            'projects-tags': reverse(project_views.ListTags.name, request=request),
+            'blog': reverse(blog_views.ListArticle.name, request=request)
         })
 
 
