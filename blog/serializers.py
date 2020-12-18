0fbdde290a4b8d1e937bf932c97512ed7dc2e038
alda27 <amunozro8970@universidadean.edu.co>
2020-12-18 16:15:38 -0500

    added serializers for blog models

---
+++ blog/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Article, Category, Tags
+
+
+class TagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
