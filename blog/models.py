d8c33e145c6d04dca3db39416985400893d77104
alda27 <amunozro8970@universidadean.edu.co>
2020-12-19 09:15:26 -0500

    removed tag model

--- blog/models.py
+++ blog/models.py
@@ -5,20 +5,20 @@ import uuid
 
 # Create your models here.
 
-class Tags(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # I used uuid to make easier the id
-    # field
-    name = models.CharField(max_length=100, blank=False, unique=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = 'Tags'
-        verbose_name_plural = 'Tags'
-        ordering = ('-date_created',)
-
-    def __str__(self):
-        return self.name
+# class Tags(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # I used uuid to make easier the id
+#     # field
+#     name = models.CharField(max_length=100, blank=False, unique=True)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
+#
+#     class Meta:
+#         verbose_name = 'Tags'
+#         verbose_name_plural = 'Tags'
+#         ordering = ('-date_created',)
+#
+#     def __str__(self):
+#         return self.name
 
 
 class Category(models.Model):
@@ -44,7 +44,7 @@ class Article(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='articles/%Y/%m/%d/', default='articles/default.jpg')
-    tags = models.ManyToManyField(Tags, related_name='articles_tags', blank=True)
+    # tags = models.ManyToManyField(Tags, related_name='articles_tags', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles_author')
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name='articles_category')
 
