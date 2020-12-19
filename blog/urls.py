ffc4fb53863ca5951c7a0849a84f217c439bab6f
alda27 <amunozro8970@universidadean.edu.co>
2020-12-19 09:16:16 -0500

    remove tag url

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 urlpatterns = [
    path('', views.ListArticle.as_view(), name=views.ListArticle.name),
    path('article/<uuid:pk>', views.DetailArticle.as_view(), name=views.DetailArticle.name),
-   path('tags/', views.ListTags.as_view(), name=views.ListTags.name),
-   path('tags/<uuid:pk>', views.DetailTags.as_view(), name=views.DetailTags.name),
+   # path('tags/', views.ListTags.as_view(), name=views.ListTags.name),
+   # path('tags/<uuid:pk>', views.DetailTags.as_view(), name=views.DetailTags.name),
    path('category/', views.ListCategory.as_view(), name=views.ListCategory.name),
    path('category/<uuid:pk>', views.DetailCategory.as_view(), name=views.DetailCategory.name),
 ]
